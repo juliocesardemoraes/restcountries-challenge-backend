7849be5113e54100aa67c8c0430b676133f2b37b
julio cesar <jcmcf2012@gmail.com>
2023-10-23 22:00:21 -0300

    fix: cors error

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ const router = require("./countriesApi/controller");
 const app = express();
 const cors = require("cors");
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // You can specify the allowed origins here
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(router);
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.listen(3000);
